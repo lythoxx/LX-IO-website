8e88ff7449d4781796feec7587498f10fe0381b0
lythox <max.broudre@gmx.de>
2024-08-08 11:20:45 +0200

    more than featured projects possible in search

--- static/js/dropdown.js
+++ static/js/dropdown.js
@@ -4,10 +4,15 @@ const dropdownMenu = document.querySelector(".list-unstyled")
 inputField.addEventListener('input', (event) => {
     // Filter the dropdown for possible values
     for (let item of dropdownValues) {
-        if (!item.innerHTML.toLowerCase().includes(event.target.value.toLowerCase())) {
+        if (!item.children[0].childNodes[2].textContent.toLowerCase().includes(event.target.value.toLowerCase())) {
             item.style = 'display: none;'
         } else {
             item.style = 'display: block;'
+        }
+    }
+    if (event.target.value.length === 0) {
+        for (let i = 3; i < dropdownValues.length; i++) {
+            dropdownValues[i].style = 'display: none;'
         }
     }
 })
